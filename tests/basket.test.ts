50cf6f1fcb65885fe04453c25b6ba223d31f84b4
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-01-26 19:30:31 +0100

    test: it 'adds books to basket'

--- tests/basket.test.ts
+++ tests/basket.test.ts
@@ -1,14 +1,44 @@
 import {setupBasket, basket} from "../src/basket"
 
 describe('Book basket', () => {
-    beforeEach(() => { 
+    beforeEach(() => {
+        document.body.innerHTML = `
+        <div>
+          <h1>	
+          &#128218;</h1>
+          <h1>Book basket</h1>
+          <div id="basket" class="card">
+            <button id="book-1" class="book" type="button">Book 1</button>
+            <button id="book-2" class="book" type="button">Book 2</button>
+            <button id="book-3" class="book" type="button">Book 3</button>
+            <button id="book-4" class="book" type="button">Book 4</button>
+            <button id="book-5" class="book" type="button">Book 5</button>
+            </div>
+            <button id="total" type="button">Calculate total</button>
+        </div>
+        `;
         setupBasket()
      })
+    //  afterEach(() => { 
+    //     basket = undefined
+        
+    //   })
     it('adds books to basket', () => { 
+        const book1Button = document.querySelector<HTMLButtonElement>('#book-1');
+        book1Button && book1Button.click()
+        book1Button && book1Button.click()
+        const book5Button = document.querySelector<HTMLButtonElement>('#book-5');
+        book5Button && book5Button.click()
+
+        expect(basket["book-1"]).toEqual(2);
+        expect(basket["book-5"]).toEqual(1);
+
         console.table(basket)
      })
     // it('removes books from basket');
-    // it('adds prices to total');
+    it('adds prices to total', () => { 
+        
+     })
     // it('calculates and discounts savings percentages');
     // it(`doesn't apply discount to duplicate books`);
  })
