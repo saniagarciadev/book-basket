72c17ccbff1b440e96ae725b2f3c366a0b361096
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-01-26 21:55:00 +0100

    test apply discount and fix cleanup after each test

--- src/basket.ts
+++ src/basket.ts
@@ -4,12 +4,19 @@ type Basket = Record<BookID, Amount>;
 
 let basket: Basket;
 let total: number;
+let bundle: number;
+
+const discounts: { [num: number]: number } = {
+  2: 5,
+  3: 10,
+  4: 20,
+  5: 25
+}
 
 export function setupBasket() {
    const addToBasket = (book: BookID) => {
     basket = {...basket}
     let amount = basket[book] ? basket[book] + 1 : 1;
-
     basket = {...basket, [book]: amount}
     return basket;
 }
@@ -17,8 +24,14 @@ export function setupBasket() {
 const getTotal = () => { 
   if (!basket) return;
   total = Object.values(basket).reduce(function(a, b){
-    return a + b * 8;
-}, 0);
+    return a + b;
+  }, 0) * 8;
+  bundle = Object.keys(basket).length;
+  if (bundle > 1) {
+    const discount: number = bundle * 8 / 100 * discounts[bundle]
+    total -= discount;
+  }
+
  }
 
   const bookButtons = document.querySelectorAll<HTMLButtonElement>('.book');
@@ -32,7 +45,13 @@ const getTotal = () => {
 
  const calculateButton = document.querySelector<HTMLButtonElement>('#total');
 
- calculateButton && calculateButton.addEventListener('click', getTotal)
+ calculateButton && calculateButton.addEventListener('click', () => getTotal())
 };
+
+export function clearBasket() {
+  basket = {}
+  total = 0
+  bundle = 0
+}
 
 export {basket, total}
--- tests/basket.test.ts
+++ tests/basket.test.ts
@@ -1,29 +1,36 @@
-import {setupBasket, basket, total} from "../src/basket"
+import {setupBasket, clearBasket, basket, total} from "../src/basket"
 
 describe('Book basket', () => {
-    beforeEach(() => {
-        document.body.innerHTML = `
-        <div>
-          <h1>	
-          &#128218;</h1>
-          <h1>Book basket</h1>
-          <div id="basket" class="card">
-            <button id="book-1" class="book" type="button">Book 1</button>
-            <button id="book-2" class="book" type="button">Book 2</button>
-            <button id="book-3" class="book" type="button">Book 3</button>
-            <button id="book-4" class="book" type="button">Book 4</button>
-            <button id="book-5" class="book" type="button">Book 5</button>
-            </div>
-            <button id="total" type="button">Calculate total</button>
+    document.body.innerHTML = `
+    <div>
+      <h1>	
+      &#128218;</h1>
+      <h1>Book basket</h1>
+      <div id="basket" class="card">
+        <button id="book-1" class="book" type="button">Book 1</button>
+        <button id="book-2" class="book" type="button">Book 2</button>
+        <button id="book-3" class="book" type="button">Book 3</button>
+        <button id="book-4" class="book" type="button">Book 4</button>
+        <button id="book-5" class="book" type="button">Book 5</button>
         </div>
-        `;
+        <button id="total" type="button">Calculate total</button>
+    </div>
+    `;
+
+    const book1Button = document.querySelector<HTMLButtonElement>('#book-1');
+    const book5Button = document.querySelector<HTMLButtonElement>('#book-5');
+    const calculateButton = document.querySelector<HTMLButtonElement>('#total');
+
+    beforeEach(() => {
         setupBasket()
      })
-    it('adds books to basket', () => { 
-        const book1Button = document.querySelector<HTMLButtonElement>('#book-1');
+     afterEach(() => {
+        clearBasket()
+        document.body.innerHTML = "";
+     })
+    it('adds books to basket', () => {
         book1Button && book1Button.click()
         book1Button && book1Button.click()
-        const book5Button = document.querySelector<HTMLButtonElement>('#book-5');
         book5Button && book5Button.click()
 
         expect(basket["book-1"]).toEqual(2);
@@ -31,15 +38,21 @@ describe('Book basket', () => {
 
      })
     // TODO it('removes books from basket');
-    it.only('adds prices to total', () => { const book1Button = document.querySelector<HTMLButtonElement>('#book-1');
-    book1Button && book1Button.click()
-    book1Button && book1Button.click()
-    book1Button && book1Button.click()
-        const calculateButton = document.querySelector<HTMLButtonElement>('#total');
+    it('adds prices to total', () => {
+        book1Button && book1Button.click()
+        book1Button && book1Button.click()
+        book1Button && book1Button.click()
         calculateButton && calculateButton.click()
+
         expect(total).toEqual(24);
 
      })
-    // it('calculates and discounts savings percentages');
+    it('calculates and discounts savings percentages', () => {
+        book1Button && book1Button.click()
+        book5Button && book5Button.click()
+        calculateButton && calculateButton.click()
+        expect(total).toEqual(15.2);
+    });
     // it(`doesn't apply discount to duplicate books`);
+    // TODO it(`accepts more than one discount bundle per basket`);
  })
