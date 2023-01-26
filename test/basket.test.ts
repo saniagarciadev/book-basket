c66fffa3cddf145a5bb9124029ee044e45322859
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-01-26 14:22:44 +0100

    install jest and write test declarations

---
+++ test/basket.test.ts
@@ -0,0 +1,6 @@
+describe('Book basket', () => {
+    it('adds books to basket');
+    it('adds prices to total');
+    it('calculates and discounts savings percentages');
+    it(`doesn't apply discount to duplicate books`);
+ })
